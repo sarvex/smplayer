bc26ac387b83bffb8b6f8982d047894b512f710a
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-10-11 08:44:54 +0000

    Added Spanish translation from r175

---
+++ web/forums/lang/Spanish/topic.php
@@ -0,0 +1,27 @@
+<?php
+
+// Language definitions used in viewtopic.php
+$lang_topic = array(
+
+'Post reply'		=>	'Responder',
+'Topic closed'		=>	'Tema cerrado',
+'From'				=>	'Ubicación',				// User location
+'Note'				=>	'Nota',				// Admin note
+'Website'			=>	'Web',
+'Guest'				=>	'Invitado',
+'Online'			=>	'Conectado',
+'Offline'			=>	'Desconectado',
+'Last edit'			=>	'Última edición por',
+'Report'			=>	'Reportar',
+'Delete'			=>	'Borrar',
+'Edit'				=>	'Editar',
+'Quote'				=>	'Citar',
+'Is subscribed'		=>	'Actualmente estás suscrito a este tema',
+'Unsubscribe'		=>	'Cancelar suscripción',
+'Subscribe'			=>	'Suscribirme a este tema',
+'Quick post'		=>	'Mensaje rápido',
+
+'Link separator'	=>	' | ',	// The text that separates links in posts (website, e-mail, report, edit etc.)
+'Mod controls'		=>	'Controles de moderador'
+
+);
