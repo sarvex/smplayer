78a9308ae3a16dac6ac6700f16da2463c4c35ab2
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-10-12 01:38:43 +0000

    Added Catalan

---
+++ web/forums/lang/Catalan/misc.php
@@ -0,0 +1,68 @@
+<?php
+
+// Language definitions used in various scripts
+$lang_misc = array(
+
+'Mark read redirect'		=>	'Tots els temes i fòrums han estat marcats com a llegits. Redirigint &#8230;',
+'Mark forum read redirect'	=>	'All topics in this forum have been marked as read. Redirecting &#8230;', // MOD: MARK TOPICS AS READ
+
+// Send e-mail
+'Form e-mail disabled'		=>	'L\'usuari a qui tracteu d\'enviar un correu ha deshabilitat el formulari de correu.',
+'No e-mail subject'			=>	'Heu d\'introduir un assumpte.',
+'No e-mail message'			=>	'Heu d\'introduir un missatge.',
+'Too long e-mail message'	=>	'Els missatges no poden tenir una llargaria superior a 65535 caràcters (64 KB).',
+'E-mail sent redirect'		=>	'Correu electrònic enviat. Redirigint &#8230;',
+'Send e-mail to'			=>	'Envia correu a',
+'E-mail subject'			=>	'Assumpte',
+'E-mail message'			=>	'Missatge',
+'E-mail disclosure note'	=>	'Per favor adoneu-se que usant aquest formulari, el receptor coneixerà la vostra adreça de correu electrònic.',
+'Write e-mail'				=>	'Escriviu i envieu el vostre missatge de correu',
+
+// Report
+'No reason'					=>	'Heu d\'introduir una raó.',
+'Report redirect'			=>	'Missatge denunciat. Redirigint &#8230;',
+'Report post'				=>	'Denunciar missatge',
+'Reason'					=>	'Raó',
+'Reason desc'				=>	'Per favor introduïu breument una raó de perquè esteu denunciant aquest missatge',
+
+// Subscriptions
+'Already subscribed'		=>	'Ja esteu subscrit a aquest tema.',
+'Subscribe redirect'		=>	'La vostra subscripció ha estat afegida. Redirigint &#8230;',
+'Not subscribed'			=>	'No esteu subscrit a aquest tema.',
+'Unsubscribe redirect'		=>	'La vostra subscripció ha estat esborrada. Redirigint &#8230;',
+
+// General forum and topic moderation
+'Moderate'					=>	'Modera',
+'Select'					=>	'Selecció',	// the header of a column of checkboxes
+'Move'						=>	'Mou',
+'Delete'					=>	'Esborra',
+
+// Moderate forum
+'Open'						=>	'Obri',
+'Close'						=>	'Tanca',
+'Move topic'				=>	'Mou tema',
+'Move topics'				=>	'Mou temes',
+'Move legend'				=>	'Seleccioneu destí per al trasllat',
+'Move to'					=>	'Mou a',
+'Leave redirect'			=>	'Deixa tema(es) de redirecció',
+'Move topic redirect'		=>	'Tema mogut. Redirigint &#8230;',
+'Move topics redirect'		=>	'Temes moguts. Redirigint &#8230;',
+'Confirm delete legend'		=>	'Per favor confirmeu l\'esborrat',
+'Delete topics'				=>	'Esborra temes',
+'Delete topics comply'		=>	'Esteu segur de voleu esborrar els temes seleccionats?',
+'Delete topics redirect'	=>	'Temes esborrats. Redirigint &#8230;',
+'Open topic redirect'		=>	'Tema obert. Redirigint &#8230;',
+'Open topics redirect'		=>	'Temes oberts. Redirigint &#8230;',
+'Close topic redirect'		=>	'Tema tancat. Redirigint &#8230;',
+'Close topics redirect'		=>	'Temes tancats. Redirigint &#8230;',
+'No topics selected'		=>	'Heu de seleccionar almenys un tema per a moure/esborrar/obrir/tancar.',
+'Stick topic redirect'		=>	'Tema marcat com a permanent. Redirigint &#8230;',
+'Unstick topic redirect'	=>	'Tema desmarcat com a permanent. Redirigint &#8230;',
+
+// Delete multiple posts in topic
+'Delete posts'				=>	'Esborra missatges',
+'Delete posts comply'		=>	'Esteu segur de voles esborrar els missatges seleccionats?',
+'Delete posts redirect'		=>	'Missatges esborrats. Redirigint &#8230;',
+'No posts selected'			=>	'Heu de seleccionar almenys un missatge per a ser esborrat.'
+
+);
