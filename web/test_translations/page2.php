5ac1e1f98a955f26c8c42248ef776e338f3dc50b
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-10-15 01:13:26 +0000

    Added a larger text for testing in page2.php

--- web/test_translations/page2.php
+++ web/test_translations/page2.php
@@ -16,7 +16,14 @@ echo "<p>";
 tr("This is another test page.");
 
 echo "<p>";
+tr("<b>SMPlayer</b> intends to be a complete front-end for 
+<a href=\"http://www.mplayerhq.hu/\">MPlayer</a>, from basic features like 
+playing videos, DVDs, and VCDs to more advanced features like support 
+for MPlayer filters and more.");
+
+echo "<center>";
 tr("<a href=\"%1\">Go back</a>", "index.php?lang=".$lang);
+echo "</center>";
 ?>
 
 </body>
