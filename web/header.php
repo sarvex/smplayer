9ef321396d2abef489566b01d6386a8c033fdbcf
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-10-17 10:27:59 +0000

    Simplified header.php

--- web/header.php
+++ web/header.php
@@ -39,44 +39,17 @@ function header_set_section($s) {
 	echo "<div id=\"header_links\">\n";
 	echo "<ul>\n";
 
-	$act_tab = " id=\"active_tab\"";
-	$act_color = " id=\"text_black\">";
+	header_print_section(get_tr("Main"), "index.php?tr_lang=".$tr_lang, ($s=="main"));
 
-	echo "<li"; if ($s=="main") echo $act_tab;
-	echo "><a href=\"index.php?tr_lang=".$tr_lang."\"><span";
-	if ($s=="main") echo $act_color; else echo ">";
-	tr("Main");
-	echo "</a></span></li>\n";
+	header_print_section(get_tr("Screenshots"), "screenshots.php?tr_lang=".$tr_lang, ($s=="screenshots"));
 
-	echo "<li"; if ($s=="screenshots") echo $act_tab;
-	echo "><a href=\"screenshots.php?tr_lang=".$tr_lang."\"><span";
-	if ($s=="screenshots") echo $act_color; else echo ">";
-	tr("Screenshots");
-	echo "</span></a></li>\n";
+	header_print_section(get_tr("Downloads"), "downloads.php?tr_lang=".$tr_lang, ($s=="downloads"));
 
-	echo "<li"; if ($s=="downloads") echo $act_tab;
-	echo "><a href=\"downloads.php?tr_lang=".$tr_lang."\"><span";
-	if ($s=="downloads") echo $act_color; else echo ">";
-	tr("Downloads");
-	echo "</span></a></li>\n";
+	header_print_section(get_tr("Forum"), "forums/index.php", ($s=="forums"));
 
-	echo "<li"; if ($s=="forums") echo $act_tab;
-	echo "><a href=\"forums/index.php\"><span";
-	if ($s=="forums") echo $act_color; else echo ">";
-	tr("Forums");
-	echo "</span></a></li>\n";
+	header_print_section(get_tr("Bug Tracking"), "http://sourceforge.net/tracker/?group_id=185512&atid=913573", ($s=="bugs"));
 
-	echo "<li"; if ($s=="bugs") echo $act_tab;
-	echo "><a href=\"http://sourceforge.net/tracker/?group_id=185512&atid=913573\"><span";
-	if ($s=="bugs") echo $act_color; else echo ">";
-	tr("Bug Tracking");
-	echo "</span></a></li>\n";
-
-	echo "<li"; if ($s=="features") echo $act_tab;
-	echo "><a href=\"http://sourceforge.net/tracker/?group_id=185512&atid=913576\"><span";
-	if ($s=="features") echo $act_color; else echo ">";
-	tr("Feature Requests");
-	echo "</span></a></li>\n";
+	header_print_section(get_tr("Feature Requests"), "http://sourceforge.net/tracker/?group_id=185512&atid=913576", ($s=="features"));
 
 	header_print_section(get_tr("Documentation"), "documentation.php?tr_lang=".$tr_lang, ($s=="documentation"));
 
