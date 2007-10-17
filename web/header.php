840f050da4bedeef5e69a77bf3d8f23b5f124324
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-10-17 10:36:18 +0000

    Simplified more header.php

--- web/header.php
+++ web/header.php
@@ -13,32 +13,34 @@ function header_print_section($name, $link, $is_active) {
 
 function header_set_section($s) {
 	global $tr_lang;
+?>
+	<div id="languages">
+	<div id="languages_links">
+	<ul>
+	<li><a href="index.php?tr_lang=nl\">Nederlands</a> |&nbsp</li>
+	<li><a href="index.php">English</a> |&nbsp</li>
+	<li><a href="index.php?tr_lang=fr">Français</a> |&nbsp</li>
+	<li><a href="index.php?tr_lang=es">Español</a></li>
+	<li><a href="index.php?tr_lang=ja">日本語</a></li>
+	</ul>
+	</div>
+	</div>
 
-	echo "<div id=\"languages\">\n";
-	echo "<div id=\"languages_links\">\n";
-	echo "<ul>\n";
-	echo "<li><a href=\"index.php?tr_lang=nl\">Nederlands</a> |&nbsp</li>\n";
-	echo "<li><a href=\"index.php\">English</a> |&nbsp</li>\n";
-	echo "<li><a href=\"index.php?tr_lang=fr\">Français</a> |&nbsp</li>\n";
-	echo "<li><a href=\"index.php?tr_lang=es\">Español</a></li>\n";
-	echo "<li><a href=\"index.php?tr_lang=ja\">日本語</a></li>\n";
-	echo "</ul>\n";
-	echo "</div>\n";
-	echo "</div>\n";
-
-	echo "<div id=\"header\">\n";
-	echo "<br><br>\n";
-	echo "<span style=\"font-size: 26px\">\n";
-
+	<div id="header">
+	<br><br>
+	<span style="font-size: 26px">
+<?php
 	echo "<a href=\"index.php?tr_lang=".$tr_lang."\">";
 	tr("The SMPlayer Project");
-	echo "</a></span>";
+?>
+	</a></span>
 
-	echo "<div id=\"navigation\">\n";
-	echo "<div id=\"navigation_box\">\n";
-	echo "<div id=\"header_links\">\n";
-	echo "<ul>\n";
+	<div id="navigation">
+	<div id="navigation_box">
+	<div id="header_links">
+	<ul>
 
+<?php
 	header_print_section(get_tr("Main"), "index.php?tr_lang=".$tr_lang, ($s=="main"));
 
 	header_print_section(get_tr("Screenshots"), "screenshots.php?tr_lang=".$tr_lang, ($s=="screenshots"));
@@ -52,13 +54,14 @@ function header_set_section($s) {
 	header_print_section(get_tr("Feature Requests"), "http://sourceforge.net/tracker/?group_id=185512&atid=913576", ($s=="features"));
 
 	header_print_section(get_tr("Documentation"), "documentation.php?tr_lang=".$tr_lang, ($s=="documentation"));
+?>
+	</ul>
 
-	echo "</ul>\n";
+	</div>
+	</div>
+	</div>
 
-	echo "</div>\n";
-	echo "</div>\n";
-	echo "</div>\n";
-
-	echo "</div>\n";
+	</div>
+<?php
 }
 ?>
