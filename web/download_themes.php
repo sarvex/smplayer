ec25f49820947f0327b58d4bd62083926f60d3e5
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-10-23 01:51:51 +0000

    Fixed the link in download_themes.php

--- web/download_themes.php
+++ web/download_themes.php
@@ -3,6 +3,6 @@
 $FILE="smplayer-themes-0.1.13.tar.bz2";
 $URL="http://downloads.sourceforge.net/smplayer/smplayer-themes-0.1.13.tar.bz2";
 
-echo "<a href=\"download/$URL\">$FILE</a> (2 MB)";
+echo "<a href=\"$URL\">$FILE</a> (2 MB)";
 
 ?>
