b906a1b554ec97661c5bfa64a0454856bd947fa5
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-10-24 01:25:35 +0000

    Updated download_windows.php

--- web/download_windows.php
+++ web/download_windows.php
@@ -1,8 +1,7 @@
 <?
 
-$FILE="smplayer_0.5.60_win32-setup.exe";
 $URL="http://downloads.sourceforge.net/smplayer/smplayer_0.5.60_win32-setup.exe";
 
-echo "<a href=\"$URL\">$FILE</a> (18 MB)";
+print_link($URL, 18688047);
 
 ?>
