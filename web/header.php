ff0d4e40f6f50665bc7bf5578e7dbea04c5a7e3f
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-11-09 12:20:20 +0000

    Added the Polish translation

--- web/header.php
+++ web/header.php
@@ -37,7 +37,8 @@ function header_set_section($s, $rel_path="") {
 	print_language_link($file, "English", "en", $query);
 	print_language_link($file, "Français", "fr", $query);
 	print_language_link($file, "Español", "es", $query);
-	print_language_link($file, "日本語", "ja", $query, true);
+	print_language_link($file, "日本語", "ja", $query);
+	print_language_link($file, "Polski", "pl", $query, true);
 ?>
 	</ul>
 	</div>
