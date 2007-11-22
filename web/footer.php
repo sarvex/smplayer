4485fca23717d0aaf14090bbae2f0b8f517f5ce4
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-11-22 01:52:04 +0000

    Added the sourceforge logo

--- web/footer.php
+++ web/footer.php
@@ -1,3 +1,7 @@
 <div id="footer">
+<!-- 
 &copy The SMPlayer Project
+<br>
+-->
+<a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=185512&amp;type=2" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a>
 </div>
