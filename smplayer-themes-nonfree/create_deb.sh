6772cf4c658bcf9772729e69f87892ba015e86a4
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2008-06-09 07:51:16 +0000

    create_deb.sh creates now only the deb package

--- smplayer-themes-nonfree/create_deb.sh
+++ smplayer-themes-nonfree/create_deb.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
  
 ln -s debian-rvm debian
-dpkg-buildpackage -rfakeroot
+
+#dpkg-buildpackage -rfakeroot
+
+# This should be faster:
+rm build-stamp
+fakeroot debian/rules build
+fakeroot debian/rules binary
