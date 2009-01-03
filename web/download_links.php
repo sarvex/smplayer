c91168de6685ceb2ec48bc9dfe42313ca3d581e1
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2009-01-03 16:39:52 +0000

    Updated the link for smplayer-portable

--- web/download_links.php
+++ web/download_links.php
@@ -32,7 +32,7 @@ function download_windows_7z_link() {
 }
 
 function download_windows_portable_link() {
-	return create_link("smplayer-portable-0.6.5.1-2.7z", 12296954 );
+	return create_link("smplayer-portable-0.6.6.7z", 12497595 );
 }
 
 
