07413975b85ba2c8d37643ed943af94e0fc06b89
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2009-07-07 22:32:59 +0000

    Removed the French translation (only a few texts were translated)

--- web/header.php
+++ web/header.php
@@ -36,8 +36,8 @@ function header_set_section($s, $rel_path="") {
 <?php
 	//print_language_link($file, "Nederlands", "nl", $query);
 	print_language_link($file, "English", "en", $query);
-        print_language_link($file, "Italiano", "it", $query);
-	print_language_link($file, "Français", "fr", $query);
+	print_language_link($file, "Italiano", "it", $query);
+	//print_language_link($file, "Français", "fr", $query);
 	print_language_link($file, "Español", "es", $query);
 	print_language_link($file, "Deutsch", "de", $query);
 	print_language_link($file, "日本語", "ja", $query);
