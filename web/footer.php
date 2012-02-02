2ba4d029f094f240f8c946d2961955704f6393a5
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-02-02 17:02:12 +0000

    Add Lithuanian translation

--- web/footer.php
+++ web/footer.php
@@ -33,7 +33,8 @@ function print_languages() {
 	print_language_link($file, "中文", "zh", $query);
 	print_language_link($file, "Magyar", "hu", $query);
 	print_language_link($file, "Português", "pt", $query);
-	print_language_link($file, "Suomi", "fi", $query, true);
+	print_language_link($file, "Suomi", "fi", $query);
+	print_language_link($file, "Lietuvių", "lt",  $query, true);
 }
 ?>
 
