a3464f412d68758f42443aef9b27d09d6c6e8c02
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-02-22 03:01:54 +0000

    Add spanish translation

---
+++ tests/ytbrowser/src/translations/ytbrowser_es.ts
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="../configdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="20"/>
+        <source>Recording</source>
+        <translation>Grabación</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="28"/>
+        <source>&amp;Save to this folder</source>
+        <translation>&amp;Grabar en esta carpeta</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="45"/>
+        <source>&amp;Quality</source>
+        <translation>&amp;Calidad</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="59"/>
+        <location filename="../configdialog.cpp" line="113"/>
+        <source>Normal (360p)</source>
+        <translation>Normal (360p)</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="64"/>
+        <location filename="../configdialog.cpp" line="113"/>
+        <source>HD (720p)</source>
+        <translation>HD (720p)</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="69"/>
+        <location filename="../configdialog.cpp" line="113"/>
+        <source>Full HD (1080p)</source>
+        <translation>Full HD (1080p)</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="90"/>
+        <source>&amp;Format</source>
+        <translation>&amp;Formato</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="101"/>
+        <source>MP4</source>
+        <translation>MP4</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.ui" line="106"/>
+        <source>Flv</source>
+        <translation>Flv</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.cpp" line="127"/>
+        <source>Low Quality (226p)</source>
+        <translatorcomment>Baja Calidad (226p)</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../configdialog.cpp" line="127"/>
+        <source>Medium Quality (360p)</source>
+        <translation>Calidad Media (360p)</translation>
+    </message>
+    <message>
+        <location filename="../configdialog.cpp" line="127"/>
+        <source>High Quality (480p)</source>
+        <translation>Alta Calidad (480p)</translation>
+    </message>
+</context>
+<context>
+    <name>FileChooser</name>
+    <message>
+        <location filename="../filechooser.cpp" line="54"/>
+        <source>Click to select a file or folder</source>
+        <translation>Haz clic para seleccionar un fichero o carpeta</translation>
+    </message>
+</context>
+<context>
+    <name>YTDialog</name>
+    <message>
+        <location filename="../ytdialog.cpp" line="204"/>
+        <source>YouTube Browser - SMPlayer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="227"/>
+        <source>&amp;Next</source>
+        <translation>&amp;Siguiente</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="231"/>
+        <source>&amp;Previous</source>
+        <translation>&amp;Anterior</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="241"/>
+        <source>Settings</source>
+        <translation>Opciones</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="373"/>
+        <source>Most Relevant</source>
+        <translation>Más Relevante</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="377"/>
+        <source>Most Recent</source>
+        <translation>Más Reciente</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="381"/>
+        <location filename="../ytdialog.cpp" line="389"/>
+        <source>Most Viewed</source>
+        <translation>Más Visto</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="385"/>
+        <source>Most Popular</source>
+        <translation>Más Popular</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="393"/>
+        <source>Most Rated</source>
+        <translation>Mejor Calificado</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="404"/>
+        <source>Most Popular Videos on YouTube%1</source>
+        <translation>Vídeos Más Populares en YouTube%1</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="405"/>
+        <source>Most Rated Videos on YouTube%1</source>
+        <translation>Vídeos Mejor Calificados en YouTube%1</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="406"/>
+        <source>Most Viewed Videos on YouTube%1</source>
+        <translation>Vídeos Más Vistos en  YouTube%1</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="409"/>
+        <source>YouTube%2 results for &quot;%1&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="520"/>
+        <source>Error: Could not connect to Youtube Server.</source>
+        <translation>Error: No se puede conectar al servidor de Youtube</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="526"/>
+        <source>No videos found</source>
+        <translation>No se han encontrado vídeos</translation>
+    </message>
+    <message>
+        <location filename="../ytdialog.cpp" line="528"/>
+        <source>No videos found for &quot;%1&quot;</source>
+        <translation>No se han encontrado vídeos para &quot;%1&quot;</translation>
+    </message>
+</context>
+</TS>
