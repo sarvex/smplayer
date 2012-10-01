2814cf78687216736494cd91ecead96c8b8a5787
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-10-01 00:59:43 +0000

    Small footer

--- web/footer.php
+++ web/footer.php
@@ -1,44 +1,33 @@
 <footer>
 
 <div class="row">
-<div class="span3">
+<div class="span1">
 </div>
 
-<div class="span5">
-<table>
-<tr>
-<td colspan="2">
-<h4><?php tr("Other links:"); ?></h4>
-</td>
-<tr>
-<td>
-<ul>
-<li><a href="http://sourceforge.net/donate/index.php?group_id=185512"><?php tr("Donate"); ?></a></li>
-<li><a href="http://smplayer.sourceforge.net/forum/"><?php tr("Forum"); ?></a></li>
-<li><a href="http://sourceforge.net/tracker/?group_id=185512&amp;atid=913573"><?php tr("Bug Tracking"); ?></a></li>
-</td>
-<td>
-<li><a href="http://sourceforge.net/tracker/?group_id=185512&amp;atid=913576"><?php tr("Feature Requests"); ?></a></li>
-<li><a href="http://smplayer.wiki.sourceforge.net/"><?php tr("Wiki"); ?></a></li>
+<div class="span9">
+<p>
+<b><?php tr("Other links:");?></b>
+&bull; <a href="http://sourceforge.net/donate/index.php?group_id=185512"><?php tr("Donate"); ?></a> &nbsp;
+&bull; <a href="http://smplayer.sourceforge.net/forum/"><?php tr("Forum"); ?></a> &nbsp;
+&bull; <a href="http://sourceforge.net/tracker/?group_id=185512&amp;atid=913573"><?php tr("Bug Tracking"); ?></a> &nbsp;
+&bull; <a href="http://sourceforge.net/tracker/?group_id=185512&amp;atid=913576"><?php tr("Feature Requests"); ?></a> &nbsp;
+&bull; <a href="http://smplayer.wiki.sourceforge.net/"><?php tr("Wiki"); ?></a> &nbsp;
+&bull;
 <?php
 if ($site=="sourceforge")
-	echo "<li><a href=\"http://smplayer.berlios.de\">". get_tr("Mirror") ."</a></li>\n";
+	echo "<a href=\"http://smplayer.berlios.de\">". get_tr("Mirror") ."</a>\n";
 else
-	echo "<li><a href=\"http://smplayer.sourceforge.net\">". get_tr("Mirror") ."</a></li>\n";
+	echo "<a href=\"http://smplayer.sourceforge.net\">". get_tr("Mirror") ."</a>\n";
 ?>
-</ul>
-</td>
-</tr>
-</table>
+&nbsp;
+</p>
 </div>
 
-<div class="span1"></div>
-
-<div class="span3">
-<br><br>
+<div class="span1">
 <a href="http://sourceforge.net/donate/index.php?group_id=185512"><img src="http://images.sourceforge.net/images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" /></a>
+</div>
 
-<br><br>
+<div class="span1">
 <?php
 include("site_logo.php");
 ?>
