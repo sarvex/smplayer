5809d832c267cfac513c237bf7240ed8aaeebdf2
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-10-04 21:31:30 +0000

    Modal window for the smtube screenshot

--- web/smtube.php
+++ web/smtube.php
@@ -5,6 +5,19 @@ echo "<body>\n";
 print_menu(1);
 ?>
 
+<div id="screenshot" class="modal hide fade in" style="display: none;">
+	<div class="modal-header">
+		<a class="close" data-dismiss="modal">×</a>
+		<h3><?php tr("SMTube Screenshot"); ?></h3>
+	</div>
+	<div class="modal-body">
+		<center><img src="images/screenshots/smtube.png"></center>
+	</div>
+	<div class="modal-footer">
+		<a href="#" class="btn" data-dismiss="modal"><?php tr("Close"); ?></a>
+	</div>
+</div>
+
 <div class="container-fluid">
 
 <h1>SMTube</h1>
@@ -19,7 +32,7 @@ This browser allows to search Youtube videos but it's also capable of downloadin
 "Ori Rejwan");
 ?>
 <p>
-<center><img src="images/screenshots/thumbs/th_smtube.png"></center>
+<center><a data-toggle="modal" href="#screenshot"><img src="images/screenshots/thumbs/th_smtube.png"></a></center>
 
 <p>
 <?php tr("Several improvements have been done:"); ?>
