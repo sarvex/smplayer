e3b30f9f7df58ddcab7302a184a44991944b03e9
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-10-15 00:02:37 +0000

    Update clean_windows.bat

--- umplayer/trunk/clean_windows.bat
+++ umplayer/trunk/clean_windows.bat
@@ -1,9 +1,17 @@
 cd getrev
 mingw32-make distclean
 
+cd ..\zlib
+mingw32-make -fwin32\makefile.gcc clean
+
 cd ..\src
 mingw32-make distclean
 
 cd ..
-del src\translations\smplayer_*.qm
 
+del src\translations\umplayer_*.qm
+del src\object_script.umplayer.Release
+del src\object_script.umplayer.Debug
+del src\svn_revision.h
+rd src\release
+rd src\debug
