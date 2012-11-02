671cb5d87e42170749e99f442878d037734311ac
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-11-02 22:44:23 +0000

    Changes in contact

--- web/contact.php
+++ web/contact.php
@@ -8,19 +8,40 @@ print_menu(0);
 <div class="container-fluid">
 <h1><?php tr("Contact us"); ?></h1>
 <p>
-<b>Ricardo Villalba</b> &lt;smplayer.dev@gmail.com&gt; &lt;rvm@users.sourceforge.net&gt; (developer, webmaster, ubuntu packages)
-<br>
-<b>redxii</b> &lt;redxii@users.sourceforge.net&gt; (windows packages, mplayer builds for windows)
+You can send your suggestions, patches, translations or whatever regarding 
+smplayer, to 
+<b>Ricardo Villalba</b> &lt;smplayer.dev@gmail.com&gt;
 
 <h2>Support</h2>
 <p>
 You can get help in our <a href="http://smplayer.sourceforge.net/forum/">forum</a>.
-
 <p>
 Report bugs in our <a href="http://sourceforge.net/tracker/?group_id=185512&atid=913573">bug tracker</a>.
-
 <p>
 You can request new features in our <a href="http://sourceforge.net/tracker/?group_id=185512&atid=913576">request tracker</a>.
+
+<h2>Credits</h2>
+<p>
+<b>Ricardo Villalba</b> (main developer, webmaster, ubuntu packages, spanish translation)
+<br>
+<b>redxii</b> (windows packages, mplayer builds for windows)
+<br>
+<b>Charles Barcza</b> (smplayer logo). Conversion to svg by <b>akovia</b>.
+
+<h3>Translators</h3>
+<p>
+Many people worked on translations for smplayer. You can see the
+full list in the <i>about</i> dialog.
+<br>
+Here is a list of the most active translators:
+<ul>
+<li>nardog (japanese)</li>
+<li>smarquespt (portuguese)</li>
+<li>Xabier Aramendi (basque)</li>
+<li>gymka (lithuanian)</li>
+<li>bendihua (simplified chinese)</li>
+</ul>
+
 
 <h2>Twitter</h2>
 <p>
@@ -32,6 +53,16 @@ You can request new features in our <a href="http://sourceforge.net/tracker/?gro
 <a href="http://smplayer.sourceforge.net">smplayer.sourceforge.net</a>
 <br>
 <a href="http://smplayer.berlios.de">smplayer.berlios.de</a>
+
+<h2>License</h2>
+<p>
+<pre>
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+</pre>
+
 
 </div> <!-- container -->
 
