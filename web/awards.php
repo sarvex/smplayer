76de7c91f0cc9c82b9cdd265017786a13f245dce
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-11-27 10:14:20 +0000

    Move some images

--- web/awards.php
+++ web/awards.php
@@ -1,5 +1,5 @@
-<a target="_blank" href="http://www.softpedia.com/progClean/SMPlayer-Clean-68718.html"><img src="images/softpedia_free_award_f.gif"></a>
+<a target="_blank" href="http://www.softpedia.com/progClean/SMPlayer-Clean-68718.html"><img src="images/sites/softpedia.gif"></a>
 <br><br>
-<a target="_blank" href="http://smplayer.findmysoft.com/"><img src="images/review2_5_SMPlayer_award.png"></a>
+<a target="_blank" href="http://smplayer.findmysoft.com/"><img src="images/sites/findmysoft.png"></a>
 <br><br>
 <a target="_blank" href="http://smplayer.en.uptodown.com/"><img src="images/sites/up_to_down.png"></a>
