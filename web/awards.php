097e0123b1f542c665fec9107c15b19abcb108a9
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-11-27 10:09:53 +0000

    Add link to uptodown

--- web/awards.php
+++ web/awards.php
@@ -1,3 +1,5 @@
 <a target="_blank" href="http://www.softpedia.com/progClean/SMPlayer-Clean-68718.html"><img src="images/softpedia_free_award_f.gif"></a>
 <br><br>
 <a target="_blank" href="http://smplayer.findmysoft.com/"><img src="images/review2_5_SMPlayer_award.png"></a>
+<br><br>
+<a target="_blank" href="http://smplayer.en.uptodown.com/"><img src="images/sites/up_to_down.png"></a>
