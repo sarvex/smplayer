bfb91cba6ce87553bcaf86be0eaa15baead677ba
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-12-07 22:45:22 +0000

    Another review

--- web/media_reviews.php
+++ web/media_reviews.php
@@ -7,16 +7,21 @@
 	print_quote("SMPlayer - a Fantastic Multimedia Player for Windows and Linux",
 				"http://voices.yahoo.com/smplayer-fanastic-multimedia-player-windows-1972216.html",
 				"Eric Fleming",
-				"voices.yahoo.com");
+				"voices.yahoo.com", 3);
 
 	print_quote("SMPlayer: Heavy concentration of brilliant features for media playback",
 				"http://www.filecluster.com/reviews/092011/smplayer-heavy-concentration-of-brilliant-features-for-media-playback/",
 				"Lizzy Nolan",
-				"filecluster.com");
+				"filecluster.com", 3);
 
 	print_quote("The Best Video Player for Linux",
 				"http://lifehacker.com/5866656/the-best-video-player-for-linux",
 				"Whitson Gordon",
-				"lifehacker.com");
+				"lifehacker.com", 3);
+
+	print_quote("SMPlayer – Probably the best media player in the world!",
+				"http://reinep.wordpress.com/2010/12/07/probably-the-best-media-player-in-the-world/",
+				"Ray",
+				"reinep.wordpress.com", 3);
 	?>
 </div>
