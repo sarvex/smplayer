8db4ce47e1a7b858af7454b873987db57daa87f2
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2013-01-15 02:06:59 +0000

    Improve the footer

--- web/footer.php
+++ web/footer.php
@@ -7,31 +7,38 @@
 <div class="span9">
 <p>
 <b><?php tr("Other links:");?></b>
-<table class"table">
-<tr>
-<td>
-<ul>
-<li><?php echo "<a href=\"latest.php?tr_lang=$tr_lang\">". get_tr("Latest changes") ."</a>"; ?></li>
-<li><a href="http://smplayer.sourceforge.net/forum/"><?php tr("Forum"); ?></a></li>
+<div class="row-fluid">
+<div class="span3">
+<ul class="icons">
+<li><i class="icon-exclamation-sign"></i>
+<?php echo "<a href=\"latest.php?tr_lang=$tr_lang\">". get_tr("Latest changes") ."</a>"; ?></li>
+<li><i class="icon-user"></i>
+<a href="http://smplayer.sourceforge.net/forum/"><?php tr("Forum"); ?></a></li>
 <!-- <li><?php echo "<a href=\"faq.php?tr_lang=$tr_lang\">". get_tr("FAQ") ."</a>"; ?></li> -->
-<li><?php echo "<a href=\"reviews.php?tr_lang=$tr_lang\">". get_tr("Reviews") ."</a>"; ?></li>
-<li><?php echo "<a href=\"presskit.php?tr_lang=$tr_lang\">". get_tr("Press Kit") ."</a>"; ?></li>
+<li><i class="icon-comments"></i>
+<?php echo "<a href=\"reviews.php?tr_lang=$tr_lang\">". get_tr("Reviews") ."</a>"; ?></li>
+<li><i class="icon-circle"></i>
+<?php echo "<a href=\"presskit.php?tr_lang=$tr_lang\">". get_tr("Press Kit") ."</a>"; ?></li>
 </ul>
-</td>
+</div> <!-- span3 -->
 
-<td>
-<ul>
-<li><a href="http://sourceforge.net/tracker/?group_id=185512&amp;atid=913573"><?php tr("Bug Tracking"); ?></a></li>
-<li><a href="http://sourceforge.net/tracker/?group_id=185512&amp;atid=913576"><?php tr("Feature Requests"); ?></a></li>
-<li><?php echo "<a href=\"smtube.php?tr_lang=$tr_lang\">SMTube</a>"; ?></li>
+<div class="span3">
+<ul class="icons">
+<li><i class="icon-circle"></i>
+<a href="http://sourceforge.net/tracker/?group_id=185512&amp;atid=913573"><?php tr("Bug Tracking"); ?></a></li>
+<li><i class="icon-circle"></i>
+<a href="http://sourceforge.net/tracker/?group_id=185512&amp;atid=913576"><?php tr("Feature Requests"); ?></a></li>
+<li><i class="icon-circle"></i>
+<?php echo "<a href=\"smtube.php?tr_lang=$tr_lang\">SMTube</a>"; ?></li>
 <!-- <li><?php echo "<a href=\"umplayer.php?tr_lang=$tr_lang\">UMPlayer</a>"; ?></li> -->
-<li><a href="http://smplayer.info/blog/"><?php tr("Blog"); ?></a></li>
+<li><i class="icon-circle"></i>
+<a href="http://smplayer.info/blog/"><?php tr("Blog"); ?></a></li>
 </ul>
-</td>
+</div> <!-- span3 -->
 
-<td>
-<ul>
-<li>
+<div class="span4">
+<ul class="icons">
+<li><i class="icon-circle"></i>
 <?php
 if ($site=="sourceforge")
 	echo "<a href=\"http://smplayer.berlios.de\">". get_tr("Mirror") ."</a>\n";
@@ -39,32 +46,29 @@ else
 	echo "<a href=\"http://smplayer.info\">". get_tr("Mirror") ."</a>\n";
 ?>
 </li>
-<li><?php echo "<a href=\"contact.php?tr_lang=$tr_lang\">". get_tr("Contact us") ."</a>"; ?></li>
+<li><i class="icon-envelope"></i>
+<?php echo "<a href=\"contact.php?tr_lang=$tr_lang\">". get_tr("Contact us") ."</a>"; ?></li>
 <!-- <li><a href="http://smplayer.wiki.sourceforge.net/"><?php tr("Wiki"); ?></a></li> -->
-<li><a target="_blank" href="http://code.google.com/p/mplayer-for-windows/"><?php tr("MPlayer builds for Windows"); ?></a></li>
-<li>
+<li><i class="icon-circle"></i>
+<a target="_blank" href="http://code.google.com/p/mplayer-for-windows/"><?php tr("MPlayer builds for Windows"); ?></a></li>
+<li><i class="icon-twitter"></i>
 <?php
-echo "<a target=\"_blank\" href=\"https://twitter.com/smplayer_dev\" title=\"" .get_tr("Follow us on Twitter") ."\"><i class=\"icon-twitter\"></i> Twitter</a>\n";
+echo "<a target=\"_blank\" href=\"https://twitter.com/smplayer_dev\" title=\"" .get_tr("Follow us on Twitter") ."\">Twitter</a>\n";
 ?>
 </li>
-</td>
-</tr>
-</table>
-</p>
-</div>
+</div> <!-- span3 -->
+
+</div> <!-- span9 -->
+</div> <!-- row -->
 
 <div class="span2">
-<p>
-<table class"table">
-<tr>
-<td>
+<div class="row-fluid">
+<br>
 <?php
 include("site_logo.php");
 ?>
-</td>
-</tr>
-</table>
-</div>
+</div> <!-- row -->
+</div> <!-- span2 -->
 
 </div> <!-- row -->
 
