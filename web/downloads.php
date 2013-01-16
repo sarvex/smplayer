55918f7c74a2c5b5faabfd5adf77a35ff8039efb
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2013-01-16 00:24:05 +0000

    Rewrite the download page

--- web/downloads.php
+++ web/downloads.php
@@ -14,129 +14,119 @@ include_once("download_links.php");
 <div class="span9">
 
 <!-- WINDOWS -->
-<h2>Windows</h2>
-<table>
-<tr>
-<td><img src="iconos/kpackage.png" alt="*"></td>
-<td>
-<?php
-if (0) {
-$random = rand(0,9);
-if ($random < 7) {
-	echo download_windows_full_link(get_tr("Click here to download SMPlayer %1", "0.8.2"));
-	echo " [<a href=\"http://download.cnet.com/SMPlayer/3000-2139_4-10645077.html?part=dl-&subj=dl&tag=button\">";
-	tr("cnet mirror");
-	echo "</a>]";
+<div class="well">
+<div class="row-fluid">
 
-	echo " [<a href=\"http://www.softpedia.com/dyn-postdownload.php?p=68718&t=4&i=1\">";
-	tr("softpedia mirror");
-	echo "</a>]";
-} else {
-	echo "<a href=\"http://download.cnet.com/SMPlayer/3000-2139_4-10645077.html?part=dl-&subj=dl&tag=button\"><b>";
-	tr("Click here to download SMPlayer %1", "0.8.2");
-	echo "</b></a>\n";
-}
-}
-else {
-	echo download_windows_full_link(get_tr("Click here to download SMPlayer %1", "0.8.3"));
-}
-echo "<br>";
-tr("This package contains smplayer, themes, translations, mplayer %1 and 
-the YouTube browser (smtube).","<i>r35425</i>");
-echo "<br>";
-echo "<a data-toggle=\"modal\" href=\"#installer\" class=\"btn btn-mini\">". get_tr("More info") ."</a>\n";
-?>
-</td>
-</tr>
+<div class="span2">
+<img src="images/win-icon.png">
+</div> <!-- span2 -->
 
-<?php
-if (0) {
-echo "<tr>";
-echo "<td><img src=\"iconos/kpackage.png\" alt=\"*\"></td>";
-echo "<td>";
-echo "<a href=\"http://sourceforge.net/projects/smplayer/files/Unstable/beta/smplayer-0.8.1.4724-x86.exe/download\"><b>smplayer-0.8.1.4724-x86.exe</b></a>";
-echo "<br>";
-tr("Test a beta of the upcoming version 0.8.2. Features: new skinnable UI and a new mplayer build with a lot of fixes.");
-echo "<br>";
-tr("In case you find any problem, please report it in %1.", "<a href=\"http://smplayer.sourceforge.net/forum/\">". get_tr("our forum") ."</a>");
-echo "</td>";
-echo "</tr>";
-}
-?>
-
-<tr>
-<td><img src="iconos/package.png" alt="*"></td>
-<td>
-<?php
-tr("There are available other packages (like a portable version), %1.", download_all_link(get_tr("here")));
-echo "<br>";
-tr("If you wish you can also try the %1.", 
-"<a href=\"http://sourceforge.net/projects/smplayer/files/Unstable/\"><b>". get_tr("development version") ."</b></a>");
-?>
-</td></tr>
-</table>
-
-<?php
-if (0) {
-echo "<div class=\"alert alert-block\">";
-echo "<strong>";
-tr("<b>Notice:</b> on the first playback, a font cache will be 
-created (necessary for OSD and subtitles). This can take up to 10 
-or 20 seconds. This is only done once.");
-echo "</strong>";
-echo "</div>";
-}
-?>
+<div class="span10">
+<div class="media">
+	<a class="pull-left" href="#">
+		<img class="media-object" src="images/installer-icon.png">
+	</a>
+	<div class="media-body">
+		<h4 class="media-heading">
+		<?php 
+		echo download_windows_full_link(get_tr("Click here to download SMPlayer %1", "0.8.3"));
+		?>
+		</h4>
+		<?php
+		tr("This package contains smplayer, themes, translations, mplayer %1 and the Youtube&trade; browser.","<i>r35425</i>");
+		//echo "<br>";
+		echo " ";
+		echo "<a data-toggle=\"modal\" href=\"#installer\" class=\"btn btn-mini\">". get_tr("More info") ."</a>\n";
+		?>
+	</div>
+	<br>
+	<a class="pull-left" href="#">
+		<img class="media-object" src="iconos/kpackage.png">
+	</a>
+	<div class="media-body">
+		<h4 class="media-heading"><?php tr("Alternative packages"); ?></h4>
+		<?php
+		tr("There are available other packages (like a portable version), %1.", download_all_link(get_tr("here")));
+		echo "<br>";
+		tr("If you wish you can also try the %1.", 
+		"<a href=\"http://sourceforge.net/projects/smplayer/files/Unstable/\"><b>". get_tr("development version") ."</b></a>");
+		?>
+	</div>
+</div>
+</div> <!-- span10 -->
+</div> <!-- row -->
+</div> <!-- well -->
 
 <!-- LINUX -->
-<h2>Linux</h2>
-<table>
-<tr>
-<td><img src="iconos/ubuntu.png" alt="*"></td>
-<td>
-<?php
-echo "<b>"; tr("Packages for Ubuntu:"); echo "</b><br>";
-tr("To install smplayer, just run these commands in a terminal:");
-?>
+<div class="well">
+<div class="row-fluid">
+
+<div class="span2">
+<img src="images/tux-icon.png">
+</div> <!-- span2 -->
+
+<div class="span10">
+
+<div class="media">
+	<a class="pull-left" href="#">
+		<img class="media-object" src="images/ubuntu-icon.png">
+	</a>
+	<div class="media-body">
+		<h4 class="media-heading"><?php tr("Packages for Ubuntu:"); ?></h4>
+		<?php tr("To install smplayer, just run these commands in a terminal:"); ?>
 <pre>
 sudo add-apt-repository ppa:rvm/smplayer
 sudo apt-get update
 sudo apt-get install smplayer smtube smplayer-themes smplayer-skins
 </pre>
-</td>
-</tr>
-
-<tr><td><img src="iconos/package.png" alt="*"></td>
-<td>
-<?php
-echo "<a href=\"https://sourceforge.net/apps/mediawiki/smplayer/index.php?title=Contributed_Packages\">" .
-     "<b>". get_tr("Contributed packages for other distros") . "</b></a>";
-echo "<br>";
-tr("List of packages contributed by other people.");
-?>
-</td>
-</tr>
-</table>
+	</div>
+	<br>
+	<a class="pull-left" href="#">
+		<img class="media-object" src="iconos/package.png">
+	</a>
+	<div class="media-body">
+		<h4 class="media-heading">
+			<a href="https://sourceforge.net/apps/mediawiki/smplayer/index.php?title=Contributed_Packages">
+			<?php tr("Contributed packages for other distros"); ?></a>
+		</h4>
+		<?php tr("List of packages contributed by other people."); ?>
+	</div>
+</div>
+</div> <!-- span10 -->
+</div> <!-- row -->
+</div> <!-- well -->
 
 <!-- SOURCES -->
-<h2><?php tr("Sources"); ?></h2>
-<table>
-<tr>
-<td><img src="iconos/package.png" alt="*"></td>
-<td>
-<?php
-//echo "<b>"; tr("Sources:"); echo "</b><br>";
-echo download_src_link();
-echo "<br>";
-echo download_smtube_link();
-echo "<br>";
-echo download_themes_src_link();
-echo "<br>";
-echo download_skins_src_link();
-?>
-</td>
-</tr>
-</table>
+<div class="well">
+<div class="row-fluid">
+
+<div class="span2">
+<img src="images/source-icon.png">
+</div> <!-- span2 -->
+
+<div class="span10">
+
+<div class="media">
+	<a class="pull-left" href="#">
+		<img class="media-object" src="iconos/package.png">
+	</a>
+	<div class="media-body">
+		<h4 class="media-heading"><?php tr("Sources"); ?></h4>
+		<?php
+		echo download_src_link();
+		echo "<br>";
+		echo download_smtube_link();
+		echo "<br>";
+		echo download_themes_src_link();
+		echo "<br>";
+		echo download_skins_src_link();
+		?>
+	</div>
+</div>
+</div> <!-- span10 -->
+</div> <!-- row -->
+</div> <!-- well -->
+
 </div> <!-- span9 -->
 
 <div class="span3">
