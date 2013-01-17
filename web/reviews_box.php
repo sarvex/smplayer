0a52d1093c6eac7720df196008f2408b1b903669
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2013-01-17 10:07:53 +0000

    Format texts

--- web/reviews_box.php
+++ web/reviews_box.php
@@ -25,7 +25,7 @@ function add_quote($text, $link, $author, $source, $active=false) {
 	else
 	if ($source == "snapfiles") { $source = "Snapfiles"; $link = "http://www.snapfiles.com/get/smplayer.html"; }
 
-	//echo "<br>";
+	echo "<br><br>";
 	//echo "&mdash; ";
 	echo "<small>$author - <cite>";
 	if ($link != "") echo "<a href=\"$link\">$source</a>"; else echo "$source";
@@ -57,10 +57,10 @@ function enable_carousel($interval=4000){
 				"http://reinep.wordpress.com/2010/12/07/probably-the-best-media-player-in-the-world/",
 				"Ray", "reinep.wordpress.com");
 
-	add_quote("SMPlayer is the universal media player solution for Windows and Linux. ".
+	add_quote("SMPlayer is the universal media player solution for Windows and Linux.<br><br> ".
 				"While many applications strive to meet the needs of a universal audience; ".
 				"to be a platform independent software solution, SMPlayer rather ".
-				"defines the very notion of cross-platform software. ".
+				"defines the very notion of cross-platform software.<br><br> ".
 				"The GUI is exceptionally maintained-- familiar user controls are ".
 				"further supported by several, realistic icon themes-- whether launched ".
 				"in Win or Lin. The user-friendly front-end only complements what's under ".
@@ -68,63 +68,63 @@ function enable_carousel($interval=4000){
 				"SMPlayer a truly versatile choice for all multi-media enjoyment.", "",
 				"MojoRyssen", "sf");
 
-	add_quote("Best Player ever used. Hi, Recently i have started using SMPlayer ".
+	add_quote("Best Player ever used.<br><br> Hi, Recently i have started using SMPlayer ".
 				"and looking out the features and they way it load quickly for playing ".
 				"videos doesn't need any Codecs/patches for playing it, Hats off to the ".
-				"Developers... Excellent Product for Free of Cost.", "",
+				"Developers...<br><br> Excellent Product for Free of Cost.", "",
 				"Alberto Jack", "softonic");
 
 	add_quote("It's really worth to download and start using SMPlayer. ".
 				"It's very intuitive and easy to use. After starting use this ".
 				"apps in a Linux (Ubuntu 12.04) environment I no more have messages ".
-				"about missing codecs... I saw in a great quality of image and sound ".
+				"about missing codecs...<br><br> I saw in a great quality of image and sound ".
 				"all format videos. And if we want to have subtitles we just asked ".
 				"in the menu to find it online and it’s easy and fast to get ".
 				"(only a few seconds, depending on the web connection) and starts ".
-				"immediately synchronized with the movie/video. AMAZING!!! ".
+				"immediately synchronized with the movie/video. AMAZING!!!<br><br> ".
 				"Today, I start using it on Windows and I highly recommend it to ".
 				"all people, like me, are beginners! Enjoy!", "",
 				"celeste.duque.7", "softonic");
 
 	add_quote("SMPlayer is the one that just plays everything on my PC, ".
-				"including the oldest legacy formats and the newest codecs. ".
+				"including the oldest legacy formats and the newest codecs.<br><br> ".
 				"It's easy to use, has a nice GUI, it just works, and has many ".
-				"options and settings to configure, though not too many. ;) ".
+				"options and settings to configure, though not too many. ;)<br><br> ".
 				"I prefer it to VLC, which is of course very good too, ".
 				"but which has many expert settings that I don't understand, ".
-				"and which is buggy in version 2.0.0. Keep up the excellent work!", "",
+				"and which is buggy in version 2.0.0.<br><br> Keep up the excellent work!", "",
 				"malungu", "sf");
 
-	add_quote("In my opinion by miles the best Videoplayer available to those using linux. ".
+	add_quote("In my opinion by miles the best Videoplayer available to those using linux.<br><br> ".
 				"By using MPlayer as the backend you don't have to mess around with additional codecs ".
-				"and what not and you can enjoy full hardware acceleration if you have a NVIDIA videocard. ".
+				"and what not and you can enjoy full hardware acceleration if you have a NVIDIA videocard.<br><br> ".
 				"SMPlayer makes all the powerful options MPlayer offers actually usuable and ".
-				"is one of few programs that allows you to do something useful with the mouse thumb buttons. ".
+				"is one of few programs that allows you to do something useful with the mouse thumb buttons.<br><br> ".
 				"The preferred audio/subtitles function made me use it under windows as well ".
-				"instead of WMC, as its lack for it greatly annoyed me. ".
+				"instead of WMC, as its lack for it greatly annoyed me.<br><br> ".
 				"Big thanks to its developer(s?)!", "",
 				"Axeia", "sf");
 
 	add_quote("This is THE best player for linux. Do not waste your time with VLC, ".
 				"SMPlayer is faster, has many more little interesting options if you poke it ".
-				"and is reliable for anything you will ever need to play.<br>".
-				"It NEVER crashes. It NEVER says 'can't play this blah blah blah'. ".
+				"and is reliable for anything you will ever need to play.<br><br> ".
+				"It NEVER crashes. It NEVER says 'can't play this blah blah blah'.<br><br> ".
 				"Simply amazing. Many thanks to the developers. ", "",
 				"Tukos", "usc");
 
 	add_quote("Move over VLC, there's a new kid in town. I was a big supporter of VLC since it had ".
 				"so many more features than Windows Media Player. However there have been several ".
-				"bugs in it recently that made me (and many others) look for something else. ".
+				"bugs in it recently that made me (and many others) look for something else.<br><br> ".
 				"SMPlayer not only fit the bill, it surpassed VLC in every way. It can sync ".
 				"audio - a vital feature of VLC. It loads and plays faster. It can instantly ".
-				"play in full screen, which many media players including VLC have problems with in Win 7. ".
+				"play in full screen, which many media players including VLC have problems with in Win 7.<br><br> ".
 				"I do recommend choosing to custom install and checking off the additional codecs ".
 				"- can never have enough of those. And I personally turned off several logs and ".
 				"playlists to save drive space - but you may want the custom ini files that can save ".
-				"playback settings per video - another powerful feature if needed! It works better than ".
+				"playback settings per video - another powerful feature if needed!<br><br> It works better than ".
 				"any other video player I've tried, I prefer the interface and find it's controls ".
 				"and menus very easy to use, it is faster, and so far it plays everything with ".
-				"ease. What more could you want?", "",
+				"ease.<br><br> What more could you want?", "",
 				"Klatuu", "snapfiles");
 
 	add_quote("I love it. It's faster than VLC and MPC (HD videos are not choppy).", "",
